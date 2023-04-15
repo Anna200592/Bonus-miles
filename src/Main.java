d7aad32faa977a0cc94cf79d3445d33f54e27643
Neto test <strokova-92@mail.ru>
2023-04-15 12:55:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        System.out.println("Цена билета"+ ticketPrice);
+        int oneMili = 20;
+        int bonusMiles = (ticketPrice/oneMili);
+        System.out.println("Итого бонусных милей"+ bonusMiles);
+
+}
