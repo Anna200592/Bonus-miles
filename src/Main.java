0f003e54eb9d832f250cf040b4cfd9a0b788a074
Neto test <strokova-92@mail.ru>
2023-04-18 22:29:54 +0300

    Revert "New commit"
    This reverts commit f39571b922b32ff6023c6ee54d94965af5002818.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 15789;
-        System.out.println("Цена билета" + ticketPrice);
+        int ticketPrice = 13676;
+        System.out.println("Цена билета"+ ticketPrice);
         int oneMili = 20;
-        int bonusMiles = (ticketPrice / oneMili);
-        System.out.println("Итого бонусных милей" + bonusMiles);
+        int bonusMiles = (ticketPrice/oneMili);
+        System.out.println("Итого бонусных милей"+ bonusMiles);
 
-    }
 }
